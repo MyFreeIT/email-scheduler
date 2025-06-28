bae0f4db2c28d5bac31aa55779d4d9735b1d4c7f
MyFreeIT <MyFreeIT@outlook.com>
2025-06-28 19:01:47 +0300

    Added Main.java.

---
+++ src/main/java/com/github/myfreeit/emailscheduler/Main.java
@@ -0,0 +1,16 @@
+package com.github.myfreeit.emailscheduler;
+
+/*
+ * Copyright (c) 2025, Denis Odesskiy. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Denis Odesskiy
+ * ("Confidential Information"). You shall not disclose such Confidential Information
+ * and shall use it only in accordance with the terms of the license agreement you
+ * entered into with Denis Odesskiy.
+ */
+
+public class Main {
+  public static void main(String[] args) {
+    MainWindow.showUi();
+  }
+}
